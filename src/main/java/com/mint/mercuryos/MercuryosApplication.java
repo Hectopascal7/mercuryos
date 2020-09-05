fba323d3f6373dbe2a4817f6d5b0db7da4bbec3e
Hectopascal7 <hectopascal7@yeah.net>
2020-09-05 14:04:35 +0800

    项目初始化。

---
+++ src/main/java/com/mint/mercuryos/MercuryosApplication.java
@@ -0,0 +1,13 @@
+package com.mint.mercuryos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercuryosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercuryosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mint/mercuryos/MercuryosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mint.mercuryos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercuryosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
